a69531119b20409a259aff59f39008532a77ca80
katieskipper <skipper.katie@gmail.com>
2013-04-29 12:24:45 -0700

    create a basic account with the money gem

--- bank.rb
+++ bank.rb
@@ -1,1 +1,26 @@
+require 'money'
 
+class BankAccount
+  attr_reader :first_name, :last_name, :created_at
+  
+  def initialize first_name, last_name
+    @first_name = first_name
+    @last_name = last_name
+    @created_at = Time.now
+    @balance = Money.new 0
+  end
+  
+  def deposit amount
+    @balance += Money.new amount * 100
+    balance
+  end
+  
+  def withdraw amount
+    @balance -= Money.new amount * 100
+    balance
+  end
+  
+  def balance
+    "#{@balance.symbol}#{@balance.to_s}"
+  end
+end
